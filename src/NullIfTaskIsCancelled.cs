2d4ea59f3c42f58caeb7dd6eb7f48af5fa93df4d
David Spörri <davepermen@hotmail.com>
2024-05-26 20:34:22 +0200

    ok then

---
+++ src/NullIfTaskIsCancelled.cs
@@ -0,0 +1,15 @@
+﻿namespace Conesoft.Tools;
+public static class NullIfTaskIsCancelled
+{
+    public static async Task<T?> NullIfCancelled<T>(this Task<T> task)
+    {
+        try
+        {
+            return await task;
+        }
+        catch (OperationCanceledException)
+        {
+            return default;
+        }
+    }
+}
