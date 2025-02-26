f1bbadce02465ef9095feb3e98cc3c8f6ecfbedc
David Spörri <davepermen@hotmail.com>
2025-02-26 20:37:05 +0100

    fixed

--- src/SerilogSupport.cs
+++ src/SerilogSupport.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 
 namespace Conesoft.Tools;
 
@@ -8,16 +9,21 @@ static class ErrorLogger
     static internal void LogError(string message, params object[] parameters) => logError(message, parameters);
 
     internal delegate void LogErrorDelegate(string message, params object[] parameters);
-    static readonly LogErrorDelegate logError = DontLogError;
+    static LogErrorDelegate logError = InitializerLogError;
     private static void DontLogError(string message, params object[] parameters) { }
 
-    static ErrorLogger()
+    private static void InitializerLogError(string message, params object[] parameters)
     {
+        var log = $"D:\\log {Random.Shared.Next()}.txt";
+        File.AppendAllLines(log, ["ErrorLogger()"]);
+        File.AppendAllLines(log, AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).Select(t => t.FullName ?? "<no name>") ?? []);
         if (AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName == "Serilog.Log") is Type type)
         {
-            var loggerInstance = type.GetProperty("Logger")?.GetValue(null) ?? null;
-            var errorMethod = loggerInstance?.GetType().GetMethod("Error", [typeof(string), typeof(object[])]);
-            logError = (errorMethod?.CreateDelegate(typeof(LogErrorDelegate), loggerInstance) as LogErrorDelegate) ?? logError;
+            File.AppendAllLines(log, [$"Type: {type.FullName}"]);
+            var method = type.GetMethod("Error", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public, [typeof(string), typeof(object[])]);
+            File.AppendAllLines(log, [$"Method: {method?.Name ?? "not found"}"]);
+            logError = method?.CreateDelegate<LogErrorDelegate>() ?? DontLogError;
+            File.AppendAllLines(log, [$"Delegate: {logError == DontLogError}"]);
         }
     }
 }
