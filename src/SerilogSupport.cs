4ce8885fd7537de5bc93a778fc6aabd5d3d8540a
David Spörri <davepermen@hotmail.com>
2025-02-26 21:21:14 +0100

    fuck

--- src/SerilogSupport.cs
+++ src/SerilogSupport.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-using System.Security.AccessControl;
 
 namespace Conesoft.Tools;
 
@@ -14,16 +13,10 @@ static class ErrorLogger
 
     private static void InitializerLogError(string message, params object[] parameters)
     {
-        var log = $"D:\\log {Random.Shared.Next()}.txt";
-        File.AppendAllLines(log, ["ErrorLogger()"]);
-        File.AppendAllLines(log, AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).Select(t => t.FullName ?? "<no name>") ?? []);
         if (AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName == "Serilog.Log") is Type type)
         {
-            File.AppendAllLines(log, [$"Type: {type.FullName}"]);
             var method = type.GetMethod("Error", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public, [typeof(string), typeof(object[])]);
-            File.AppendAllLines(log, [$"Method: {method?.Name ?? "not found"}"]);
             logError = method?.CreateDelegate<LogErrorDelegate>() ?? DontLogError;
-            File.AppendAllLines(log, [$"Delegate: {logError == DontLogError}"]);
         }
     }
 }
