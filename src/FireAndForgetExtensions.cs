bc3b752b8d56cc0e01a0861e4af2390cf948f396
David Spörri <davepermen@hotmail.com>
2025-03-20 14:35:28 +0100

    pewpew

---
+++ src/FireAndForgetExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Conesoft.Tools;
+
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class FireAndForgetExtensions
+{
+    public static void FireAndForget(this Task _) { }
+    public static void FireAndForget(this ValueTask _) { }
+}
